16e597d09209cf5542fa93f59cac7e5bd0f21e47
Rajat Pandey <rajatpandey7800@gmail.com>
2025-05-11 15:03:05 +0530

    Constants in Go

--- basics/constants/constants.go
+++ basics/constants/constants.go
@@ -2,8 +2,25 @@ package main
 import (
 	"fmt"
 )
-
+const MAXTRIES = 5
+const PI = 3.14
 func main() {
-	const MAXTRIES = 5
+	
 	fmt.Println(MAXTRIES)
+	fmt.Println(PI)
+
+	// Declare multiple constants in one line.
+	const (
+		MONDAY = 1
+		TUESDAY = 2
+		WEDNESDAY = 3
+		THURSDAY = 4
+		FRIDAY = 5
+		SATURDAY = 6
+		SUNDAY = 7
+	)
+	fmt.Println(MONDAY, TUESDAY)
+	fmt.Println(WEDNESDAY, THURSDAY)
+	fmt.Println(FRIDAY, SATURDAY)
+	fmt.Println(SUNDAY)
 }
