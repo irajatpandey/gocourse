49a83d3373e5ea7948e19357d10f8874028a05d0
Rajat Pandey <rajatpandey7800@gmail.com>
2025-05-11 16:50:02 +0530

    For Loop

---
+++ basics/loops/loops.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Simple for loop to print numbers from 1 to 5
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i) // Print the current value of i
+	}
+
+	// Declare a slice of integers
+	numbers := []int{1, 2, 3, 4, 5, 6}
+
+	// Iterate over the slice using range
+	for index, value := range numbers {
+		// Print the index and value of each element in the slice
+		fmt.Println(index, value)
+	}
+
+	// Loop to print numbers from 10 down to 1
+	for i := range 10 {
+		// Print the countdown from 10
+		fmt.Println(10 - i)
+	}
+}
